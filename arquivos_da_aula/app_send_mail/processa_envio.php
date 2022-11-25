f167430d09ff45159b1d8e1b1c409e29243c54d5
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-25 14:17:17 -0300

    ok

--- arquivos_da_aula/app_send_mail/processa_envio.php
+++ arquivos_da_aula/app_send_mail/processa_envio.php
@@ -50,12 +50,12 @@
 	$mail = new PHPMailer(true);
 	try {
 			//Server settings
-			// $mail->SMTPDebug = 2;                      //Enable verbose debug output
+			 $mail->SMTPDebug = 2;                      //Enable verbose debug output
 			$mail->isSMTP();                                            //Send using SMTP
 			$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 			$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
 			$mail->Username   = 'renatoguara2020@gmail.com';                     //SMTP username
-			$mail->Password   = 'gpx';                               //SMTP password
+			$mail->Password   = 'gp';                               //SMTP password
 			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 			$mail->Port       = 587; 
 			$mail->CharSet = 'UTF-8';
